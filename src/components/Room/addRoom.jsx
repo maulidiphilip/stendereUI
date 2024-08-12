f0dcdbe33ee1ba4bef41967bf013bfeae4da8eac
maulidiphilip <maulidiphilip@gmail.com>
2024-08-12 20:55:27 +0200

    Designed the Add Room page

--- src/components/Room/addRoom.jsx
+++ src/components/Room/addRoom.jsx
@@ -1,7 +1,8 @@
 import { useState } from "react";
-import addRoom from '../Utils/ApiFunctions'
+import addRoom from "../Utils/ApiFunctions";
+import { Link } from "react-router-dom";
 
-const addRoom = () => {
+const AddRoom = () => {
   // varibale to hold the information
   const [newRoom, setNewRoom] = useState({
     photo: null,
@@ -29,34 +30,96 @@ const addRoom = () => {
   };
 
   const handleImageChange = (e) => {
-    const selectedImage = e.target.files[0]
-		setNewRoom({ ...newRoom, photo: selectedImage })
-		setImagePreview(URL.createObjectURL(selectedImage))
-  } 
+    const selectedImage = e.target.files[0];
+    setNewRoom({ ...newRoom, photo: selectedImage });
+    setImagePreview(URL.createObjectURL(selectedImage));
+  };
 
   // function to handle the submit
   const handleSubmit = async (e) => {
-    e.preventDefault()
+    e.preventDefault();
 
     try {
-      const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
-      if (success !==undefine) {
-        setSuccessMessage("A new room created")
-        setNewRoom({photo : null, roomType : "", roomPrice : ""})
-        setImagePreview("")
-        setErrorMessage("")
+      const success = await addRoom(
+        newRoom.photo,
+        newRoom.roomType,
+        newRoom.roomPrice
+      );
+      if (success !==undefined) {
+        setSuccessMessage("A new room created");
+        setNewRoom({ photo: null, roomType: "", roomPrice: "" });
+        setImagePreview("");
+        setErrorMessage("");
       } else {
-        setErrorMessage("Error in adding room!!!!")
+        setErrorMessage("Error in adding room!!!!");
       }
     } catch (error) {
-      setErrorMessage(error.message)
+      setErrorMessage(error.message);
     }
-  }
+  };
   return (
     <>
-      <section></section>
+      <section className="container, mt-5 mb-5">
+        <div className="row justify-content-center">
+          <div className="col-md-8 col-lg-6 ">
+            <h2 className="mt-5 mb-2">Create a Room</h2>
+
+            <form action="" onSubmit={handleSubmit}>
+              <div className="mb-3">
+                <label htmlFor="roomType" className="form-label">
+                  Room Type
+                </label>
+                <div></div>
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="roomPrice" className="form-label">
+                  Room price
+                </label>
+                <input
+                  required
+                  type="number"
+                  className="form-control"
+                  id="roomPrice"
+                  name="roomPrice"
+                  value={newRoom.roomPrice}
+                  onChange={handleRoomInputChange}
+                />
+              </div>
+
+              <div className="mb-3">
+                <label htmlFor="photo" className="form-label">
+                  Room Photo
+                </label>
+                <input
+                  required
+                  name="photo"
+                  id="photo"
+                  type="file"
+                  className="form-control"
+                  onChange={handleImageChange}
+                />
+                {imagePreview && (
+                  <img
+                    src={imagePreview}
+                    alt="Preview  room photo"
+                    style={{ maxWidth: "400px", maxHeight: "400px" }}
+                    className="mb-3"
+                  ></img>
+                )}
+              </div>
+
+              <div className="d-grid gap-2 d-md-flex mt-2">
+								<button type="submit" className="btn btn-outline-primary ml-5">
+									Create Room
+								</button>
+							</div>
+            </form>
+          </div>
+        </div>
+      </section>
     </>
   );
 };
 
-export default addRoom;
+export default AddRoom;
