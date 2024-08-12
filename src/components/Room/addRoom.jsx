7ec8af8ff3bd1a2a2cec4b895df42f9d72d9b68e
maulidiphilip <maulidiphilip@gmail.com>
2024-08-12 20:35:58 +0200

    The function and room variables created

--- src/components/Room/addRoom.jsx
+++ src/components/Room/addRoom.jsx
@@ -1,10 +1,62 @@
+import { useState } from "react";
+import addRoom from '../Utils/ApiFunctions'
 
 const addRoom = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  // varibale to hold the information
+  const [newRoom, setNewRoom] = useState({
+    photo: null,
+    roomType: "",
+    roomPrice: "",
+  });
 
-export default addRoom
+  // variable to handle image previewing, because we are going to preview image b4 submitting to the database
+  const [imagePreview, setImagePreview] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const handleRoomInputChange = (e) => {
+    const name = e.target.name;
+
+    let value = e.target.value;
+    if (name === "roomPrice") {
+      if (!isNaN(value)) {
+        value = parseInt(value);
+      } else {
+        value = "";
+      }
+    }
+    setNewRoom({ ...newRoom, [name]: value });
+  };
+
+  const handleImageChange = (e) => {
+    const selectedImage = e.target.files[0]
+		setNewRoom({ ...newRoom, photo: selectedImage })
+		setImagePreview(URL.createObjectURL(selectedImage))
+  } 
+
+  // function to handle the submit
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const success = await addRoom(newRoom.photo, newRoom.roomType, newRoom.roomPrice)
+      if (success !==undefine) {
+        setSuccessMessage("A new room created")
+        setNewRoom({photo : null, roomType : "", roomPrice : ""})
+        setImagePreview("")
+        setErrorMessage("")
+      } else {
+        setErrorMessage("Error in adding room!!!!")
+      }
+    } catch (error) {
+      setErrorMessage(error.message)
+    }
+  }
+  return (
+    <>
+      <section></section>
+    </>
+  );
+};
+
+export default addRoom;
