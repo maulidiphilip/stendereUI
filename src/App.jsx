245921f5062d2fcd11b8293a52f13214e6a3cb2d
MAULIDI <maulidiphilip@gmail.com>
2024-11-19 21:42:40 +0200

    The reservation room page works properly

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import Navbar from "./components/layout/Navbar"
 import Footer from "./components/layout/Footer"
 import RoomListing from "./components/Room/RoomListing"
 import Admin from "./components/admin/Admin"
+import Checkout from "./components/booking/Checkout"
 
 function App() {
 
@@ -23,6 +24,7 @@ function App() {
           <Route path="/edit-room/:roomId" element={<EditRoom/>}/>
           <Route path="/existing-rooms" element={<ExistingRoom/>}/>
           <Route path="/add-room" element={<AddRoom/>}/>
+          <Route path="/book-room/:roomId" element={<Checkout/>}/>
           <Route path="/browse-all-rooms" element={<RoomListing/>}/>
           <Route path="/admin" element={<Admin/>}/>
         </Routes>
