7b57a85b93db895b24412ecfaeb31e9affdc2fbe
MAULIDI <maulidiphilip@gmail.com>
2024-12-16 09:22:55 +0200

    The find my booking component works, using a confirmation code

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,43 @@
-import React from "react"
-import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import "/node_modules/bootstrap/dist/js/bootstrap.min.js"
-import AddRoom from "./components/Room/addRoom"
-import ExistingRoom from "./components/Room/ExistingRoom"
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
-import Home from "./components/Home/Home"
-import EditRoom from "./components/Room/EditRoom"
-import Navbar from "./components/layout/Navbar"
-import Footer from "./components/layout/Footer"
-import RoomListing from "./components/Room/RoomListing"
-import Admin from "./components/admin/Admin"
-import Checkout from "./components/booking/Checkout"
-import BookingSuccess from "./components/booking/BookingSuccess"
-import Bookings from "./components/booking/Bookings"
+import React from "react";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "/node_modules/bootstrap/dist/js/bootstrap.min.js";
+import AddRoom from "./components/Room/addRoom";
+import ExistingRoom from "./components/Room/ExistingRoom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home/Home";
+import EditRoom from "./components/Room/EditRoom";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import RoomListing from "./components/Room/RoomListing";
+import Admin from "./components/admin/Admin";
+import Checkout from "./components/booking/Checkout";
+import BookingSuccess from "./components/booking/BookingSuccess";
+import Bookings from "./components/booking/Bookings";
+import FindBooking from "./components/booking/FindBooking";
 
 function App() {
-
   return (
     <>
-    <main>
-      <Router>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<Home/>}/>
-          <Route path="/edit-room/:roomId" element={<EditRoom/>}/>
-          <Route path="/existing-rooms" element={<ExistingRoom/>}/>
-          <Route path="/add-room" element={<AddRoom/>}/>
-          <Route path="/book-room/:roomId" element={<Checkout/>}/>
-          <Route path="/browse-all-rooms" element={<RoomListing/>}/>
-          <Route path="/admin" element={<Admin/>}/>
-          <Route path="/booking-success" element={<BookingSuccess/>}/>
-          <Route path="/existing-bookings" element={<Bookings/>}/>
-        </Routes>
-      </Router>
-      <Footer/>
-    </main>
+      <main>
+        <Router>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/edit-room/:roomId" element={<EditRoom />} />
+            <Route path="/existing-rooms" element={<ExistingRoom />} />
+            <Route path="/add-room" element={<AddRoom />} />
+            <Route path="/book-room/:roomId" element={<Checkout />} />
+            <Route path="/browse-all-rooms" element={<RoomListing />} />
+            <Route path="/admin" element={<Admin />} />
+            <Route path="/booking-success" element={<BookingSuccess />} />
+            <Route path="/existing-bookings" element={<Bookings />} />
+            <Route path="/find-booking" element={<FindBooking />} />
+          </Routes>
+        </Router>
+        <Footer />
+      </main>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Utils/ApiFunctions.js
+++ src/components/Utils/ApiFunctions.js
@@ -1,130 +1,129 @@
 import axios from "axios";
 
 export const api = axios.create({
-    baseURL : "http://localhost:9192"
-})
+  baseURL: "http://localhost:9192",
+});
 
 export const getHeader = () => {
-	const token = localStorage.getItem("token")
-	return {
-		Authorization: `Bearer ${token}`,
-		"Content-Type": "application/json"
-	}
-}
+  const token = localStorage.getItem("token");
+  return {
+    Authorization: `Bearer ${token}`,
+    "Content-Type": "application/json",
+  };
+};
 
 // this method is used to add a new room into the database or in our application
 export async function addRoom(photo, roomType, roomPrice) {
-	const formData = new FormData()
-	formData.append("photo", photo)
-	formData.append("roomType", roomType)
-	formData.append("roomPrice", roomPrice)
+  const formData = new FormData();
+  formData.append("photo", photo);
+  formData.append("roomType", roomType);
+  formData.append("roomPrice", roomPrice);
 
-	const response = await api.post("/rooms/add/new-room", formData)
-	if (response.status === 201) {
-		return true
-	} else {
-		return false
-	}
+  const response = await api.post("/rooms/add/new-room", formData);
+  if (response.status === 201) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* This function gets all room types from thee database */
 export async function getRoomTypes() {
-	try {
-		const response = await api.get("/rooms/room/types")
-		return response.data
-	} catch (error) {
-		throw new Error("Error fetching room types")
-	}
+  try {
+    const response = await api.get("/rooms/room/types");
+    return response.data;
+  } catch (error) {
+    throw new Error("Error fetching room types");
+  }
 }
 
-// function to get all the room from the backend/database 
-export async function getAllRooms(){
-	try {
-		const results = await api.get("/rooms/all-rooms")
-		return results.data
-	} catch (error) {
-		throw new Error("Error fetching rooms");
-		
-	}
+// function to get all the room from the backend/database
+export async function getAllRooms() {
+  try {
+    const results = await api.get("/rooms/all-rooms");
+    return results.data;
+  } catch (error) {
+    throw new Error("Error fetching rooms");
+  }
 }
 // this function will handle the delete options
 export async function deleteRoom(roomId) {
-	try {
-		const result = await api.delete(`/rooms/delete/room/${roomId}`)
-		return result.data
-	} catch (error) {
-		throw new Error(`Error occurred deleteing the room ${error.message}`);
-		
-	}
+  try {
+    const result = await api.delete(`/rooms/delete/room/${roomId}`);
+    return result.data;
+  } catch (error) {
+    throw new Error(`Error occurred deleteing the room ${error.message}`);
+  }
 }
 
 // update or edit the room
 export async function updateRoom(roomId, roomData) {
-	const formData = new FormData()
-	formData.append("roomType", roomData.roomType)
-	formData.append("roomPrice", roomData.roomPrice)
-	formData.append("photo", roomData.photo)
-	const response = await api.put(`/rooms/update/${roomId}`, formData)
-	return response
+  const formData = new FormData();
+  formData.append("roomType", roomData.roomType);
+  formData.append("roomPrice", roomData.roomPrice);
+  formData.append("photo", roomData.photo);
+  const response = await api.put(`/rooms/update/${roomId}`, formData);
+  return response;
 }
 
 // the dollowing function will enable us to get a single room from the database
 export async function getRoomById(roomId) {
-	try {
-		const result = await api.get(`/rooms/room/${roomId}`)
-		return result.data
-	} catch (error) {
-		throw new Error(`Error fetching room ${error.message}`);
-	}
+  try {
+    const result = await api.get(`/rooms/room/${roomId}`);
+    return result.data;
+  } catch (error) {
+    throw new Error(`Error fetching room ${error.message}`);
+  }
 }
 
 // function to book the room
 export async function bookRoom(roomId, booking) {
-	try {
-		const response = await api.post(`/bookings/room/${roomId}/booking`, booking)
+  try {
+    const response = await api.post(
+      `/bookings/room/${roomId}/booking`,
+      booking
+    );
 
-		return response.data
-	} catch (error) {
-		if (error.response && error.response.data) {
-			throw new Error(error.response.data)
-		} else {
-			throw new Error(`Error booking room : ${error.message}`);
-			
-		}
-	}
+    return response.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw new Error(error.response.data);
+    } else {
+      throw new Error(`Error booking room : ${error.message}`);
+    }
+  }
 }
 
 // get all booking from the atabase
 export async function getAllBooking() {
-	try {
-		const result = await api.get("/bookings/all-bookings")
-		return result.data
-	} catch (error) {
-		throw new Error(`Failed to get bookings: ${error.message}`);
-	}
+  try {
+    const result = await api.get("/bookings/all-bookings");
+    return result.data;
+  } catch (error) {
+    throw new Error(`Failed to get bookings: ${error.message}`);
+  }
 }
 
-// now find bookings by confirmation code 
-export async function getBookingByConfirmationCode(confirmationCode){
-	try {
-	const result = await api.get(`/bookings/confirmation/${confirmationCode}`)
-	return result.data
-	} catch (error) {
-		if(error.response && error.response.data){
-			throw new Error(error.response);
-		} else {
-			throw new Error(`Error finding booking: ${error.message}`);
-			
-		}
-	}
+// now find bookings by confirmation code
+export async function getBookingByConfirmationCode(confirmationCode) {
+  try {
+    const result = await api.get(`/bookings/confirmation/${confirmationCode}`);
+    return result.data;
+  } catch (error) {
+    if (error.response && error.response.data) {
+      throw new Error(error.response.data);
+    } else {
+      throw new Error(`Error find booking : ${error.message}`);
+    }
+  }
 }
 
 // method to cancel bookings
 export async function cancelBooking(bookingId) {
-	try {
-		const results = await api.delete(`/bookings/booking/${bookingId}/delete`)
-		return results.data
-	} catch (error) {
-		throw new Error(`Error canclling booking : ${error.message}`);
-	}
+  try {
+    const result = await api.delete(`/bookings/booking/${bookingId}/delete`);
+    return result.data;
+  } catch (error) {
+    throw new Error(`Error cancelling booking :${error.message}`);
+  }
 }
